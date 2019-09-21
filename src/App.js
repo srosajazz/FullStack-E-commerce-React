13e7a758163865801fe5c3cb27f5a514d38f7acd
Sergio Rosa <scarlosjazz@gmail.com>
2019-09-21 07:47:17 -0400

    Directory folder

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import './App.css';
-import HomePage from './pages/homepages/homepage';
+import HomePage from './pages/homepages';
 
 function App() {
   return (
--- src/pages/homepages/index.js
+++ src/pages/homepages/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import './homepage.styles.scss';
+import './styles.scss';
 
 const HomePage = () => (
   <div className="homepage">
